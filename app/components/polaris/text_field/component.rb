235c02943152bc77f5ea4a2f51395a38a03bb4e0
Dan Gamble <dan@dangamble.co.uk>
2021-03-20 10:51:20 +0000

    Used Labelled component for TextField and add Index attribute

--- app/components/polaris/text_field/component.rb
+++ app/components/polaris/text_field/component.rb
@@ -4,7 +4,11 @@ module Polaris
     class Component < Polaris::Component
       with_content_areas :connected_left, :connected_right
 
-      def initialize(form:, attribute:, placeholder: "", type: "text", error: "", label: nil, multiline: false, **args)
+      validates :index, numericality: { only_integer: true }, allow_nil: true
+
+      attr_reader :index
+
+      def initialize(form:, attribute:, placeholder: "", type: "text", error: "", label: nil, label_hidden: false, multiline: false, help_text: "", index: nil, **args)
         super
         @placeholder = placeholder
         @type = type
@@ -12,7 +16,22 @@ module Polaris
         @attribute = attribute
         @error = error
         @label = label
+        @label_hidden = label_hidden
         @multiline = multiline
+        @index = index
+      end
+
+      def labelled_attrs
+        {
+          form: @form,
+          attr: @attribute,
+          error: @error,
+          label: @label,
+          label_hidden: @label_hidden,
+          help_text: @help_text,
+          index: @index,
+        #  action
+        }
       end
 
       def input
