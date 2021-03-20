c0341aed80e0e984d9e5a414408e200d33e7522a
Dan Gamble <dan@dangamble.co.uk>
2021-03-20 09:44:13 +0000

    Add VisuallyHidden component

---
+++ app/components/polaris/visually_hidden/component.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Polaris
+  module VisuallyHidden
+    class Component < Polaris::Component
+      private
+
+        def classes
+          ["Polaris-VisuallyHidden"]
+        end
+    end
+  end
+end
