e0b45cb020f63ba2633dfa5efa1c6e2d062dadcd
Dan Gamble <dan@dangamble.co.uk>
2021-03-21 13:40:12 +0000

    Add Caption component

---
+++ app/components/caption/component.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Polaris
+  module Caption
+    class Component < Polaris::Component
+      private
+
+        def classes
+          ["Polaris-Caption"]
+        end
+    end
+  end
+end
