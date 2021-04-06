40df8cbaa278574a33730bf5b827c9b171948c2c
Dan Gamble <dan@dangamble.co.uk>
2021-04-06 16:24:23 +0100

    Add more options to the Card component
    - secondary_footer_actions (just the 1, for now)
    - footer_action_alignment

--- app/components/polaris/card/component.rb
+++ app/components/polaris/card/component.rb
@@ -4,22 +4,37 @@ module Polaris
     class Component < Polaris::Component
       include ActionHelper
 
+      ALLOWED_FOOTER_ACTION_ALIGNMENT = %w[right left]
+
       validates :primary_footer_action, type: ComplexAction, allow_nil: true
+      validates :footer_action_alignment, inclusion: {
+        in: ALLOWED_FOOTER_ACTION_ALIGNMENT,
+        message: "%{value} is not a valid footer_action_alignment"
+      }, allow_blank: true, allow_nil: true
 
       with_content_areas :header
 
-      attr_reader :primary_footer_action
+      attr_reader :primary_footer_action, :footer_action_alignment
 
-      def initialize(title: '', primary_footer_action: nil, sectioned: false, **args)
+      def initialize(
+        title: '',
+        primary_footer_action: nil,
+        secondary_footer_actions: [],
+        footer_action_alignment: 'right',
+        sectioned: false,
+        **args
+      )
         super
 
         @title = title
         @primary_footer_action = primary_footer_action
+        @secondary_footer_actions = secondary_footer_actions
+        @footer_action_alignment = footer_action_alignment
         @sectioned = sectioned
       end
 
       def footer?
-        primary_footer_action.present?
+        primary_footer_action.present? || @secondary_footer_actions.present?
       end
 
       private
