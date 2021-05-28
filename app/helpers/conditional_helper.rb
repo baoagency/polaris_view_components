a849f70c048b10db5481e817006dfff27f2f06ee
Dan Gamble <dan@bao.agency>
2021-05-28 11:10:03 +0100

    Add ConditionalHelper

---
+++ app/helpers/conditional_helper.rb
@@ -0,0 +1,9 @@
+module ConditionalHelper
+  def conditional_wrapping_element(tag, options = {}, &block)
+    if options.delete(:show)
+      content_tag(tag, capture(&block), options)
+    else
+      capture(&block)
+    end
+  end
+end
