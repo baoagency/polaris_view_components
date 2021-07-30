82a2da3b0405ee0974367b91fa3db294a6a785f7
Kirill Platonov <mail@kirillplatonov.com>
2021-07-30 08:47:44 +0300

    Fix engine loading in rails generators

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -1,7 +1,6 @@
 require_relative "boot"
 
 require "rails/all"
-require "polaris/view_components/engine"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
