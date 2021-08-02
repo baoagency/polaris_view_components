de07279f65ef29d2c2929ebfbe63846f997169ca
Kirill Platonov <mail@kirillplatonov.com>
2021-08-02 19:22:20 +0300

    Rebuild ShopifyNavigation component

---
+++ demo/test/components/previews/navigation/shopify_navigation_component_preview.rb
@@ -0,0 +1,6 @@
+class Navigation::ShopifyNavigationComponentPreview < ViewComponent::Preview
+  layout "plain"
+
+  def default
+  end
+end
---
+++ test/components/polaris/shopify_navigation_component_test.rb
@@ -0,0 +1,26 @@
+require "test_helper"
+
+class ShopifyNavigationComponentTest < Minitest::Test
+  include Polaris::ComponentTestHelpers
+
+  def test_renders_navigation
+    render_inline(Polaris::ShopifyNavigationComponent.new) do |nav|
+      nav.link(url: "/page1", active: true) { "Page 1" }
+      nav.link(url: "/page2", target: "_blank") { "Page 2" }
+    end
+
+    assert_selector ".shp-Navigation > .Polaris-Stack > ul.shp-Navigation_Items" do
+      assert_selector "li.shp-Navigation_Item", count: 2
+      assert_selector "li.shp-Navigation_Item:nth-child(1)" do
+        assert_selector "a.shp-Navigation_Link[href='/page1']" do
+          assert_selector "span.shp-Navigation_LinkText", text: "Page 1"
+        end
+      end
+      assert_selector "li.shp-Navigation_Item:nth-child(2)" do
+        assert_selector "a.shp-Navigation_Link[href='/page2'][target='_blank']" do
+          assert_selector "span.shp-Navigation_LinkText", text: "Page 2"
+        end
+      end
+    end
+  end
+end
