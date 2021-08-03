5ba33332d340a3cf43562226aa59ddcc7ac92d12
Kirill Platonov <mail@kirillplatonov.com>
2021-08-03 17:09:33 +0300

    Remove unused helpers

--- app/lib/polaris/action_helper.rb
+++ app/lib/polaris/action_helper.rb
@@ -1,24 +1,5 @@
 module Polaris
   module ActionHelper
-    def polaris_action(**args)
-      Polaris::Action.new(**args)
-    end
-
-    def polaris_complex_action(**args)
-      Polaris::ComplexAction.new(**args)
-    end
-
-    def render_action(action)
-      case action
-        when Hash
-          action = action(**action)
-      end
-
-      render Polaris::ButtonComponent.new(**action.to_h.except(:content)) do
-        action.content
-      end
-    end
-
     def render_plain_action(action)
       case action
         when Hash
@@ -27,39 +8,6 @@ module Polaris
 
       render Polaris::ButtonComponent.new(**action.to_h.except(:content), plain: true) do
         action.content
-      end
-    end
-
-    def render_complex_action_button(complex_action)
-      case complex_action
-        when Hash
-          complex_action = polaris_complex_action(**complex_action)
-      end
-
-      render Polaris::ButtonComponent.new(**complex_action.to_h.except(:content)) do
-        complex_action.content
-      end
-    end
-
-    def render_primary_complex_action_button(complex_action)
-      case complex_action
-        when Hash
-          complex_action = polaris_complex_action(**complex_action)
-      end
-
-      render Polaris::ButtonComponent.new(**complex_action.to_h.except(:content), primary: true) do
-        complex_action.content
-      end
-    end
-
-    def render_plain_complex_action_button(complex_aciton)
-      case complex_aciton
-        when Hash
-          complex_aciton = polaris_complex_action(**complex_aciton)
-      end
-
-      render Polaris::ButtonComponent.new(**complex_aciton.to_h.except(:content), plain: true) do
-        complex_aciton.content
       end
     end
   end
