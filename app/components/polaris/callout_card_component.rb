9f934da8856f75c6539bff21e04c77f6846c6035
Kirill Platonov <mail@kirillplatonov.com>
2021-08-03 14:35:18 +0300

    Rebuild CalloutCard component

---
+++ app/components/polaris/callout_card_component.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+module Polaris
+  class CalloutCardComponent < Polaris::NewComponent
+    renders_one :primary_action, Polaris::ButtonComponent
+    renders_one :secondary_action, -> (plain: true, **system_arguments) do
+      Polaris::ButtonComponent.new(plain: plain, **system_arguments)
+    end
+    renders_one :dismiss_button, -> (**system_arguments) do
+      render Polaris::ButtonComponent.new(plain: true, **system_arguments) do |button|
+        button.icon(name: "CancelSmallMinor")
+      end
+    end
+
+    def initialize(
+      title: nil,
+      illustration: nil,
+      **system_arguments
+    )
+      @title = title
+      @illustration = illustration
+
+      @system_arguments = system_arguments
+      @system_arguments[:tag] = "div"
+      @system_arguments[:classes] = class_names(
+        @system_arguments[:classes],
+        "Polaris-Card",
+      )
+    end
+
+    def container_classes
+      class_names(
+        "Polaris-CalloutCard__Container",
+        "Polaris-CalloutCard--hasDismiss": dismiss_button.present?,
+      )
+    end
+
+    def image_classes
+      class_names(
+        "Polaris-CalloutCard__Image",
+        "Polaris-CalloutCard__DismissImage": dismiss_button.present?,
+      )
+    end
+  end
+end
