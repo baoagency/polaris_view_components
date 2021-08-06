ced11b7cf618591a2f2f1d3fa799520761a5bce1
Kirill Platonov <mail@kirillplatonov.com>
2021-08-06 09:36:40 +0300

    Update engine setup

--- lib/polaris/view_components/engine.rb
+++ lib/polaris/view_components/engine.rb
@@ -11,16 +11,17 @@ module Polaris
         #{root}/app/lib
       ]
 
-      # Loads helpers into main app automatically
-      config.to_prepare do
-        ApplicationController.helper Polaris::ViewHelper
-        ApplicationController.helper Polaris::ActionHelper
-        ApplicationController.helper Polaris::ConditionalHelper
-      end
-
-      initializer "polaris_view_components.assets.precompile" do |app|
+      initializer "polaris_view_components.assets" do |app|
         if app.config.respond_to?(:assets)
           app.config.assets.precompile += %w[polaris_view_components.css]
+        end
+      end
+
+      initializer "polaris_view_components.helpers" do
+        ActiveSupport.on_load(:action_controller_base) do
+          helper Polaris::ViewHelper
+          helper Polaris::ActionHelper
+          helper Polaris::ConditionalHelper
         end
       end
     end
