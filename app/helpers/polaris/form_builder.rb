2c1b3c4881387de84cfb1b0bb9813272f6408327
Kirill Platonov <mail@kirillplatonov.com>
2021-08-10 10:43:43 +0300

    Use full error messages in form builder

--- app/helpers/polaris/form_builder.rb
+++ app/helpers/polaris/form_builder.rb
@@ -1,16 +1,19 @@
 module Polaris
   class FormBuilder < ActionView::Helpers::FormBuilder
     def polaris_text_field(method, **options, &block)
-      options[:error] ||= errors_for(method)
+      options[:error] ||= error_for(method)
+      if options[:error_hidden] && options[:error]
+        options[:error] = !!options[:error]
+      end
       @template.render(
         Polaris::TextFieldComponent.new(form: self, attribute: method, **options, &block)
       )
     end
 
-    def errors_for(method)
+    def error_for(method)
       return if object.blank?
       return unless object.errors.key?(method)
-      object.errors[method].join(', ').html_safe
+      object.errors.full_messages_for(method)&.first
     end
   end
 end
