11c1d55f09899e21f8fdf1939006bb7039b74fc7
Kirill Platonov <mail@kirillplatonov.com>
2021-10-07 15:53:29 +0300

    Add one_fourth section support to Layout

--- demo/test/components/previews/structure/layout_component_preview.rb
+++ demo/test/components/previews/structure/layout_component_preview.rb
@@ -15,6 +15,9 @@ class Structure::LayoutComponentPreview < ViewComponent::Preview
   def three_columns_with_equal_width
   end
 
+  def four_columns_with_equal_width
+  end
+
   # Use for settings pages. When settings are grouped thematically in annotated sections, the title and description on each section helps merchants quickly find the setting they’re looking for.
   def annotated_layout
   end
