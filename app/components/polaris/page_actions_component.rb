39c919e9b223b7798adb926e864766285d341b9c
Kirill Platonov <mail@kirillplatonov.com>
2021-10-14 14:00:50 +0300

    Add distribution parameter for PageActions

--- app/components/polaris/page_actions_component.rb
+++ app/components/polaris/page_actions_component.rb
@@ -2,12 +2,24 @@
 
 module Polaris
   class PageActionsComponent < Polaris::NewComponent
+    DISTRIBUTION_DEFAULT = nil
+    DISTRIBUTION_OPTIONS = [
+      DISTRIBUTION_DEFAULT,
+      :equal_spacing,
+      :leading,
+      :trailing,
+      :center,
+      :fill,
+      :fill_evenly,
+    ]
+
     renders_one :primary_action, -> (primary: true, **system_arguments) do
       Polaris::ButtonComponent.new(primary: primary, **system_arguments)
     end
     renders_many :secondary_actions, Polaris::ButtonComponent
 
-    def initialize(**system_arguments)
+    def initialize(distribution: DISTRIBUTION_DEFAULT, **system_arguments)
+      @distribution = fetch_or_fallback(DISTRIBUTION_OPTIONS, distribution, DISTRIBUTION_DEFAULT)
       @system_arguments = system_arguments
       @system_arguments[:tag] = "div"
       @system_arguments[:classes] = class_names(
@@ -18,6 +30,8 @@ module Polaris
 
     private
       def stack_distribution
+        return @distribution if @distribution.present?
+
         (primary_action.present? && secondary_actions.any?) ? :equal_spacing : :trailing
       end
   end
