328f51caa1b2e274bed0446793d9532b8b148fea
Kirill Platonov <mail@kirillplatonov.com>
2021-11-02 09:53:02 +0300

    Add SettingToggle component
    Closes #137

---
+++ app/components/polaris/setting_toggle_component.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Polaris
+  class SettingToggleComponent < Polaris::NewComponent
+    renders_one :action, -> (**system_arguments) do
+      Polaris::ButtonComponent.new(primary: !@enabled, **system_arguments)
+    end
+
+    def initialize(enabled: false, **system_arguments)
+      @enabled = enabled
+      @system_arguments = system_arguments
+    end
+
+    def system_arguments
+      @system_arguments.tap do |opts|
+        opts[:tag] = "div"
+        opts[:classes] = class_names(
+          @system_arguments[:classes],
+          "Polaris-SettingAction",
+        )
+      end
+    end
+  end
+end
---
+++ test/components/polaris/setting_toggle_component_test.rb
@@ -0,0 +1,28 @@
+require "test_helper"
+
+class AvatarComponentTest < Minitest::Test
+  include Polaris::ComponentTestHelpers
+
+  def test_enabled_setting_toggle
+    render_inline(Polaris::SettingToggleComponent.new(enabled: true)) do |toggle|
+      toggle.action { "Action" }
+      "Content"
+    end
+
+    assert_selector ".Polaris-Card > .Polaris-Card__Section > .Polaris-SettingAction" do
+      assert_selector ".Polaris-SettingAction__Setting", text: "Content"
+      assert_selector ".Polaris-SettingAction__Action > .Polaris-Button", text: "Action"
+    end
+  end
+
+  def test_disabled_setting_toggle
+    render_inline(Polaris::SettingToggleComponent.new(enabled: false)) do |toggle|
+      toggle.action { "Action" }
+      "Content"
+    end
+
+    assert_selector ".Polaris-SettingAction > .Polaris-SettingAction__Action" do
+      assert_selector ".Polaris-Button.Polaris-Button--primary", text: "Action"
+    end
+  end
+end
