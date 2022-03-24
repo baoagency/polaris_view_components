27e72c243a9444c0e2574bc06a35358e1c39b93b
Kirill Platonov <kirill@platonov.net>
2022-03-24 17:58:50 +0300

    Hide loader for unchanged Dropzones

--- app/assets/javascripts/polaris_view_components.js
+++ app/assets/javascripts/polaris_view_components.js
@@ -459,6 +459,7 @@ class Dropzone extends Controller {
   onDirectUploadsEnd=() => {
     this.enable();
     this.clearFiles();
+    if (this.acceptedFiles.length === 0) return;
     this.loaderTarget.classList.remove("Polaris--hidden");
   };
   onDirectUploadInitialize=event => {
@@ -466,6 +467,7 @@ class Dropzone extends Controller {
     const {id: id, file: file} = detail;
     const dropzone = target.closest(".Polaris-DropZone");
     if (!dropzone) return;
+    if (this.acceptedFiles.length === 0) return;
     if (this.sizeValue == "small") {
       this.clearFiles();
       this.loaderTarget.classList.remove("Polaris--hidden");
