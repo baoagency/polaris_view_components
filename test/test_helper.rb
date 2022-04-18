38052b815c3fde860705bb6c4c8e63297a06c632
Kirill Platonov <kirill@platonov.net>
2022-04-18 17:22:25 +0300

    Remove dummy app from tests (#223)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,17 +1,10 @@
 # Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
 
-require_relative "../test/dummy/config/environment"
-ActiveRecord::Migrator.migrations_paths = [File.expand_path("../test/dummy/db/migrate", __dir__)]
-ActiveRecord::Migrator.migrations_paths << File.expand_path("../db/migrate", __dir__)
+require File.expand_path("../demo/config/environment.rb", __dir__)
+
+require "minitest/autorun"
+require "rails"
 require "rails/test_help"
 require "test_helpers/component_test_helper"
 require "pry"
-
-# Load fixtures from the engine
-if ActiveSupport::TestCase.respond_to?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path("fixtures", __dir__)
-  ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
-  ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + "/files"
-  ActiveSupport::TestCase.fixtures :all
-end
