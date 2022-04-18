bcf2843da2f66323ba87550342359661972b286f
Kirill Platonov <kirill@platonov.net>
2022-04-18 14:55:09 +0300

    Add Ruby 3.1 support

--- app/components/polaris/visually_hidden_component.rb
+++ app/components/polaris/visually_hidden_component.rb
@@ -2,9 +2,6 @@
 
 module Polaris
   class VisuallyHiddenComponent < Polaris::Component
-    def initialize
-    end
-
     def call
       content_tag(:span, content, class: "Polaris-VisuallyHidden")
     end
