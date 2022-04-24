8c52288fe8838317bbd935ff8e93090fdd75379b
Kirill Platonov <kirill@platonov.net>
2022-04-24 11:12:59 +0300

    Update Lookbook (#228)

--- demo/config/application.rb
+++ demo/config/application.rb
@@ -2,6 +2,7 @@ require_relative "boot"
 
 require "rails"
 # Pick the frameworks you want:
+require "active_job/railtie"
 require "active_model/railtie"
 require "action_controller/railtie"
 require "action_view/railtie"
