92d84e0d1fd33edb4f00a66efff300ab6abb40ac
Kirill Platonov <kirill@platonov.net>
2022-05-10 15:02:49 +0300

    Add system arguments support to table's columns (#232)

--- app/components/polaris/data_table/column_component.rb
+++ app/components/polaris/data_table/column_component.rb
@@ -2,7 +2,7 @@ class Polaris::DataTable::ColumnComponent < Polaris::Component
   SORT_DEFAULT = false
   SORT_OPTIONS = [false, :asc, :desc]
 
-  attr_reader :title, :numeric, :total, :sorted, :sort_url
+  attr_reader :title, :numeric, :total, :sorted, :sort_url, :system_arguments
 
   def initialize(title, numeric: false, total: nil, sorted: SORT_DEFAULT, sort_url: nil, **system_arguments, &block)
     @title = title
@@ -11,6 +11,7 @@ class Polaris::DataTable::ColumnComponent < Polaris::Component
     @sorted = fetch_or_fallback(SORT_OPTIONS, sorted, SORT_DEFAULT)
     @sort_url = sort_url
     @block = block
+    @system_arguments = system_arguments
   end
 
   def call(row)
--- app/components/polaris/index_table/column_component.rb
+++ app/components/polaris/index_table/column_component.rb
@@ -1,10 +1,11 @@
 class Polaris::IndexTable::ColumnComponent < Polaris::Component
-  attr_reader :title, :flush
+  attr_reader :title, :flush, :system_arguments
 
   def initialize(title, flush: false, **system_arguments, &block)
     @title = title
     @flush = flush
     @block = block
+    @system_arguments = system_arguments
   end
 
   def call(row)
