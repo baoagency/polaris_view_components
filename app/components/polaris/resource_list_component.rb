453d6481ec89cde173e4743e4f47d33b8c2fd2e3
Dan Gamble <dan@dangamble.co.uk>
2022-06-03 14:39:20 +0100

    Total items count for ResourceListComponent (#240)

--- app/components/polaris/resource_list_component.rb
+++ app/components/polaris/resource_list_component.rb
@@ -5,9 +5,16 @@ module Polaris
     renders_one :filters, Polaris::FiltersComponent
 
     def initialize(
+      items: [],
+      resource_name: nil,
+      total_items_count: nil,
       wrapper_arguments: {},
       **system_arguments
     )
+      @items = items
+      @resource_name = resource_name
+      @total_items_count = total_items_count
+
       @wrapper_arguments = wrapper_arguments
       @wrapper_arguments[:tag] = "div"
       @wrapper_arguments[:classes] = class_names(
@@ -21,6 +28,20 @@ module Polaris
         @system_arguments[:classes],
         "Polaris-ResourceList"
       )
+    end
+
+    def header_title
+      count unless @total_items_count.nil?
+    end
+
+    def resource_string
+      return @resource_name[:singular] if @items.size === 1 && !@total_items_count
+
+      @resource_name[:plural]
+    end
+
+    def count
+      "Showing #{@items.size} of #{@total_items_count} #{resource_string}"
     end
   end
 end
--- test/components/polaris/resource_list_component_test.rb
+++ test/components/polaris/resource_list_component_test.rb
@@ -25,4 +25,34 @@ class ResourceListComponentTest < Minitest::Test
       assert_selector ".Polaris-ResourceList__FiltersWrapper > .Polaris-Filters"
     end
   end
+
+  def test_resource_list_with_total_items_count
+    items = [
+      {
+        url: "customers/341",
+        name: "Mae Jemison",
+        location: "Decatur, USA"
+      },
+      {
+        url: "customers/256",
+        name: "Ellen Ochoa",
+        location: "Los Angeles, USA"
+      }
+    ]
+
+    render_inline(Polaris::ResourceListComponent.new(
+      resource_name: {
+        singular: "customer",
+        plural: "customers"
+      },
+      items: items,
+      total_items_count: 50
+    )) do |resource_list|
+      "Content"
+    end
+
+    assert_selector ".Polaris-ResourceList__ResourceListWrapper" do
+      assert_selector ".Polaris-ResourceList__HeaderWrapper"
+    end
+  end
 end
