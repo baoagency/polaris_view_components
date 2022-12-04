c18b966bdeb780067bfe6894bc59c2bc9096395e
Kirill Platonov <kirill@platonov.net>
2022-12-04 10:34:44 +0300

    Switch to lookbook fork

--- demo/config/environments/development.rb
+++ demo/config/environments/development.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
-    config.cache_store = :memory_store
+    config.cache_store = :file_store, "#{root}/tmp/cache/"
     config.public_file_server.headers = {
       "Cache-Control" => "public, max-age=#{2.days.to_i}"
     }
