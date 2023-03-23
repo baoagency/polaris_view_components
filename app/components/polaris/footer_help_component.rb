3ebffa47c1f433d5be347007bba2405fbcb31323
Kirill Platonov <kirill@platonov.net>
2023-03-23 14:48:18 +0400

    Update FooterHelp component

--- app/components/polaris/footer_help_component.rb
+++ app/components/polaris/footer_help_component.rb
@@ -4,6 +4,7 @@ module Polaris
   class FooterHelpComponent < Polaris::Component
     def initialize(**system_arguments)
       @system_arguments = system_arguments
+      @system_arguments[:tag] = "div"
       @system_arguments[:classes] = class_names(
         @system_arguments[:classes],
         "Polaris-FooterHelp"
