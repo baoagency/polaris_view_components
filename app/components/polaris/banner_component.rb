0356f89b8e610aaf2351f879d6206029f3677890
Kirill Platonov <kirill@platonov.net>
2023-04-30 20:41:27 +0300

    Update Banner component

--- app/components/polaris/banner_component.rb
+++ app/components/polaris/banner_component.rb
@@ -28,7 +28,7 @@ module Polaris
     }
 
     renders_one :action, ->(**system_arguments) do
-      Polaris::ButtonComponent.new(classes: "Polaris-Banner__Button", **system_arguments)
+      Polaris::ButtonComponent.new(classes: "Polaris-Banner__Button Polaris-Banner__PrimaryAction", **system_arguments)
     end
     renders_one :secondary_action, "SecondaryAction"
     renders_one :dismiss_button, ->(**system_arguments) do
@@ -41,11 +41,13 @@ module Polaris
       status: STATUS_DEFAULT,
       within: WITHIN_DEFAULT,
       icon: nil,
+      hide_icon: false,
       title: nil,
       **system_arguments
     )
       @status = status
       @icon = icon || default_icon(status)
+      @hide_icon = hide_icon
       @title = title
 
       @system_arguments = system_arguments
