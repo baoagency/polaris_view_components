be317779eddc159c7fcf6c52258cf549dcb67f39
yshmarov <yshmarov@gmail.com>
2023-11-22 20:27:28 +0100

    tests

--- test/components/polaris/navigation_list_component_test.rb
+++ test/components/polaris/navigation_list_component_test.rb
@@ -6,25 +6,19 @@ class NavigationListComponentTest < Minitest::Test
   def test_default_navigation
     render_inline(Polaris::NavigationListComponent.new) do |navigation|
       navigation.with_item(url: "/path1", label: "Item 1", icon: "HomeMajor")
-      navigation.with_item(url: "/path2", label: "Item 2", icon: "OrdersMajor")
+      navigation.with_item(url: "/path2", label: "Item 2", icon: "OrdersMajor", selected: true)
     end
 
-    assert_selector "nav.Polaris-Navigation > .Polaris-Navigation__PrimaryNavigation.Polaris-Scrollable" do
-      assert_selector "ul.Polaris-Navigation__Section" do
-        assert_selector "li.Polaris-Navigation__ListItem", count: 2
-        assert_selector "li:nth-child(1)" do
-          assert_selector ".Polaris-Navigation__ItemWrapper" do
+    assert_selector "div.Polaris-LegacyCard" do
+      assert_selector "li.Polaris-Navigation__ListItem" do
+        assert_selector "div.Polaris-Navigation__ItemWrapper" do
+          assert_selector "div.Polaris-Navigation__ItemInnerWrapper" do
             assert_selector "a.Polaris-Navigation__Item[href='/path1']" do
-              assert_selector ".Polaris-Navigation__Icon > .Polaris-Icon"
-              assert_selector ".Polaris-Navigation__Text", text: "Item 1"
-            end
-          end
-        end
-        assert_selector "li:nth-child(2)" do
-          assert_selector ".Polaris-Navigation__ItemWrapper" do
-            assert_selector "a.Polaris-Navigation__Item[href='/path2']" do
-              assert_selector ".Polaris-Navigation__Icon > .Polaris-Icon"
-              assert_selector ".Polaris-Navigation__Text", text: "Item 2"
+              assert_selector "div.Polaris-Navigation__Icon" do
+                assert_selector "span.Polaris-Icon" do
+                  assert_selector "svg.Polaris-Icon__Svg"
+                end
+              end
             end
           end
         end
