6632986d388f3c440301a7d90842b3509d931e8e
Yaro Shm <yshmarov@gmail.com>
2023-11-23 03:40:57 +0100

    NavigationListComponent (#341)

---
+++ demo/app/previews/navigation_list_component_preview.rb
@@ -0,0 +1,4 @@
+class NavigationListComponentPreview < ViewComponent::Preview
+  def default
+  end
+end
---
+++ test/components/polaris/navigation_list_component_test.rb
@@ -0,0 +1,28 @@
+require "test_helper"
+
+class NavigationListComponentTest < Minitest::Test
+  include Polaris::ComponentTestHelpers
+
+  def test_default_navigation
+    render_inline(Polaris::NavigationListComponent.new) do |navigation|
+      navigation.with_item(url: "/path1", label: "Item 1", icon: "HomeMajor")
+      navigation.with_item(url: "/path2", label: "Item 2", icon: "OrdersMajor", selected: true)
+    end
+
+    assert_selector "div.Polaris-LegacyCard" do
+      assert_selector "li.Polaris-Navigation__ListItem" do
+        assert_selector "div.Polaris-Navigation__ItemWrapper" do
+          assert_selector "div.Polaris-Navigation__ItemInnerWrapper" do
+            assert_selector "a.Polaris-Navigation__Item[href='/path1']" do
+              assert_selector "div.Polaris-Navigation__Icon" do
+                assert_selector "span.Polaris-Icon" do
+                  assert_selector "svg.Polaris-Icon__Svg"
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
