549c384de1a3e87ec4bafdaade5c529e191e40a5
Remy Wang <remy.wang727@gmail.com>
2024-07-09 09:20:36 +0200

    Improve Frame component (#428)
    * feat: support secondary menu
    
    * fix: make detail optional
    
    * feat: support showing order
    
    * docs: update lookbook

--- app/components/polaris/top_bar/user_menu_component.rb
+++ app/components/polaris/top_bar/user_menu_component.rb
@@ -1,9 +1,10 @@
 class Polaris::TopBar::UserMenuComponent < Polaris::Component
   renders_one :avatar, Polaris::AvatarComponent
 
-  def initialize(name:, detail:, **system_arguments)
+  def initialize(name:, detail: nil, show_avatar_first: false, **system_arguments)
     @name = name
     @detail = detail
+    @show_avatar_first = show_avatar_first
     @system_arguments = system_arguments
   end
 end
--- demo/app/previews/frame_component_preview.rb
+++ demo/app/previews/frame_component_preview.rb
@@ -1,4 +1,13 @@
 class FrameComponentPreview < ViewComponent::Preview
   def default
   end
+
+  def optional_detail
+  end
+
+  def show_avatar_first
+  end
+
+  def with_secondary_menu
+  end
 end
