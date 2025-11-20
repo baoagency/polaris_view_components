8c8f39382bcf0a4f5a4581e5737e26387ede8cbe
Kirill Platonov <mail@kirillplatonov.com>
2025-11-20 22:12:25 +0100

    Fix demo site config

--- demo/config/environments/production.rb
+++ demo/config/environments/production.rb
@@ -73,6 +73,4 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-
-  config.active_record.sqlite3_production_warning = false
 end
