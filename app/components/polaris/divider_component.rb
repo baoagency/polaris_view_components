52b789222c4f607897c0758aca6dcd23789b2b0c
Justin Leger <juslleg@gmail.com>
2025-12-11 23:59:54 -0500

    Add block direction for divider component
    Similar the divider in polaris web components, add a direction property that is
    either 'inline' or 'block'. 'block' allows for vertical dividers When nothing is
    provided, 'inline' is used. Invalid values will also fallback to 'inline'.
    
    A new preview was added to display the block behaviour.

--- app/components/polaris/divider_component.rb
+++ app/components/polaris/divider_component.rb
@@ -2,9 +2,16 @@
 
 module Polaris
   class DividerComponent < Polaris::Component
+    include ActiveModel::Validations
+
+    SUPPORTED_DIRECTIONS = %w[inline block].freeze
+
+    validates :direction, inclusion: { in: SUPPORTED_DIRECTIONS }
+
     def initialize(
       border_color: "border-subdued",
       border_width: "1",
+      direction: "inline",
       **system_arguments
     )
       @system_arguments = system_arguments.tap do |args|
@@ -15,7 +22,7 @@ module Polaris
         )
         args[:style] = styles_list(
           args[:style],
-          "border-block-start": "var(--p-border-width-#{fetch_or_fallback(Tokens::Border::WIDTH_SCALE, border_width)}) solid #{border_color_value(border_color)}"
+          **border_styles(direction, border_color, border_width)
         )
       end
     end
@@ -30,6 +37,22 @@ module Polaris
       return "border-subdued" unless border_color
       return "transparent" if border_color == "transparent"
       "var(--p-color-#{fetch_or_fallback(Tokens::Color::BORDER_ALIAS, border_color)})"
+    end
+
+    def border_styles(direction, border_color, border_width)
+      border_direction = fetch_or_fallback(SUPPORTED_DIRECTIONS, direction, "inline")
+
+      if border_direction == "block"
+        {
+          "border-inline-start": "var(--p-border-width-#{fetch_or_fallback(Tokens::Border::WIDTH_SCALE, border_width)}) solid #{border_color_value(border_color)}",
+          "inline-size": "auto",
+          "min-block-size": "100%",
+        }
+      else
+        {
+          "border-block-start": "var(--p-border-width-#{fetch_or_fallback(Tokens::Border::WIDTH_SCALE, border_width)}) solid #{border_color_value(border_color)}"
+        }
+      end
     end
   end
 end
